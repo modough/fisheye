080e5217a43892bd8c4f19c01cc97329531782f4
mouhamed <mouhamed.mbaye1@outlook.com>
2022-11-15 11:53:47 +0100

    test commit with husky again

--- scripts/factories/professionalMedias.js
+++ scripts/factories/professionalMedias.js
@@ -1,31 +1,34 @@
-/* eslint-disable import/prefer-default-export */
-export default function professionalMedia (data) {
-  const { title, image, likes, video, photographerId} = data;
-  
-  const picture = `assets/medias/${photographerId}/${image}`; 
-  const file = `assets/medias/${photographerId}/${video}`;
- 
-  console.log(photographerId)
+export default function professionalMedia(data) {
+	const { title, image, likes, video, photographerId } = data;
 
-  const getUserCardDOM = () => 
-    `<div class="photoCard">
-        <div class="divImgVideo">
-          ${video ? 
-            `<video src="${file}"></video><i class="far fa-play-circle"></i>` 
-          :
-            `<img src="${picture}">`}
-        </div>
+	const picture = `assets/medias/${photographerId}/${image}`;
+	const file = `assets/medias/${photographerId}/${video}`;
 
-        <div class="bottomCard">
-          <h2>${title}</h2>
-          <span>
-            <span class="number-of-likes">${likes}</span>
-            <span><i class="fas fa-heart"></i></span>
-          </span>
-        </div>
+	console.log(photographerId);
+
+	const getUserCardDOM = () =>
+		`<div class="photoCard">
+      ${
+	video
+		? `<a href="#" class="photoCardLink">
+          <div class="divImgVideo">
+              <video src="${file}"></video>
+          </div>  
+        </a>`
+		: `<a href="#">
+          <div class="divImgVideo">  
+            <img src="${picture}">  
+          </div>  
+        </a>`
+} 
+
+      <div class="bottomCard">
+        <h2>${title}</h2>
+        <span>
+          <span class="number-of-likes">${likes}</span>
+          <i class="fas fa-heart"></i>
+        </span>
+      </div>
     </div>`;
-  return { getUserCardDOM };
+	return { getUserCardDOM };
 }
-
-
-
